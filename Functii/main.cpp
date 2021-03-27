ca20db89efdd2931d530bb900db03b3f70123b03
test <test@test.com>
2021-03-27 17:52:21 +0200

    ready to commit test

--- Functii/main.cpp
+++ Functii/main.cpp
@@ -5,13 +5,21 @@
 
 using namespace std;
 
+int hpHealedp, playerHp, hpHealedb, bossHp,
+
 int healPlayer(int playerHp)
 {
-
+    srand (time(NULL));
+    int hpHealedp = rand() % (50 - 35 + 1) + 35;
+    playerHp = playerHp + hpHealedp;
+    return playerHp;
 }
 int healBoss(int bossHp)
 {
-
+    srand (time(NULL));
+    int hpHealedb = rand() % (55 - 35 + 1) + 35;
+    bossHp = bossHp + hpHealedb;
+    return bossHp;
 }
 int damageToBoss(int bossHp)
 {
