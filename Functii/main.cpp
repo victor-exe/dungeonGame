acfd7c0025acf40d3091799f83a68bc1a99faf45
test <test@test.com>
2021-03-27 17:27:23 +0200

    first functions commit

---
+++ Functii/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int healPlayer(int playerHp)
+{
+
+}
+int healBoss(int bossHp)
+{
+
+}
+int damageToBoss(int bossHp)
+{
+
+}
+int damageToPlayer(int playerHp)
+{
+
+}
+bossDecision()
